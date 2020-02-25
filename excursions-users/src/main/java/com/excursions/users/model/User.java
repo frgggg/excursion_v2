a09bd05015a789dd5928ad8c3d92dbb942c0deac
Fedor <zhar.feda@yandex.ru>
2020-02-25 15:07:31 +0300

    init user: no docker

---
+++ excursions-users/src/main/java/com/excursions/users/model/User.java
@@ -0,0 +1,58 @@
+package com.excursions.users.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+import static com.excursions.users.validation.message.ValidationMessagesComponents.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    public static final String USER_NAME_FIELD_NAME = "name";
+    public static final int USER_NAME_LEN_MIN = 1;
+    public static final int USER_NAME_LEN_MAX = 90;
+    public static final String USER_NAME_VALIDATION_MESSAGE =
+            USER_NAME_FIELD_NAME + STRING_FIELD_NOTNULL_MIN_MAX + USER_NAME_LEN_MIN + STRING_FIELD_NOTNULL_MIN_MAX_DIVIDE + USER_NAME_LEN_MAX;
+
+    public static final String USER_COINS_FIELD_NAME = "coins";
+    public static final int USER_COINS_MIN = 0;
+    public static final String  USER_COINS_VALIDATION_MESSAGE = USER_COINS_FIELD_NAME + LONG_FIELD_NOTNULL_NOT_NEGATIVE;
+
+    public static final String USER_COINS_LAST_UPDATE_FIELD_NAME = "coinsLastUpdate";
+    public static final String  USER_COINS_LAST_UPDATE_VALIDATION_MESSAGE = USER_COINS_LAST_UPDATE_FIELD_NAME + LOCAL_DATA_TIME_FIELD_NOTNULL;
+
+    @Id
+    @SequenceGenerator(name = "users_sequence_gen", sequenceName = "users_sequence", allocationSize = 1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="users_sequence_gen")
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "name", length = USER_NAME_LEN_MAX, nullable = false)
+    @NotNull(message = USER_NAME_VALIDATION_MESSAGE)
+    @Size(min = USER_NAME_LEN_MIN, max = USER_NAME_LEN_MAX, message = USER_NAME_VALIDATION_MESSAGE)
+    private String name;
+
+    @Column(name = "coins", nullable = false)
+    @NotNull(message = USER_COINS_VALIDATION_MESSAGE)
+    @Min(value = USER_COINS_MIN, message = USER_COINS_VALIDATION_MESSAGE)
+    private Long coins;
+
+    @Column(name = "coins_last_update", nullable = false)
+    @NotNull(message = USER_COINS_LAST_UPDATE_VALIDATION_MESSAGE)
+    private LocalDateTime coinsLastUpdate;
+
+    protected User() {}
+
+    public User(String name) {
+        this.name = name;
+        coins = 0l;
+        coinsLastUpdate = LocalDateTime.now();
+    }
+}
---
+++ excursions-users/src/main/java/com/excursions/users/service/impl/UserServiceImpl.java
@@ -0,0 +1,190 @@
+package com.excursions.users.service.impl;
+
+import com.excursions.users.exception.ServiceException;
+import com.excursions.users.model.User;
+import com.excursions.users.repository.UserRepository;
+import com.excursions.users.service.ExcursionService;
+import com.excursions.users.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.ConstraintViolationException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static com.excursions.users.exception.message.UserServiceExceptionMessages.*;
+import static com.excursions.users.log.message.UserServiceLogMessages.*;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private static final String USER_CACHE_NAME = "userCache";
+
+    private static final boolean IS_UP_FLAG = true;
+    private static final boolean IS_NOT_UP_FLAG = false;
+
+    private UserRepository userRepository;
+    private UserServiceImpl self;
+    private ExcursionService excursionService;
+
+    @Lazy
+    @Autowired
+    protected UserServiceImpl(UserRepository userRepository, UserServiceImpl self, ExcursionService excursionService) {
+        this.userRepository = userRepository;
+        this.self = self;
+        this.excursionService = excursionService;
+    }
+
+    @Caching(put= {@CachePut(value= USER_CACHE_NAME, key= "#result.id")})
+    @Override
+    public User create(String name) {
+        User savedUser;
+        savedUser = saveOrUpdateUtil(new User(name));
+        log.info(USER_SERVICE_LOG_NEW_USER, savedUser);
+        return savedUser;
+    }
+
+    @Caching(put= {@CachePut(value= USER_CACHE_NAME, key= "#result.id")})
+    @Override
+    public User update(Long id, String name) {
+        User userForUpdate = self.findById(id);
+        userForUpdate.setName(name);
+        User updatedUser;
+        updatedUser = saveOrUpdateUtil(userForUpdate);
+        log.info(USER_SERVICE_LOG_UPDATE_USER, userForUpdate, updatedUser);
+        return updatedUser;
+    }
+
+    @Override
+    public List<User> findAll() {
+        log.info(USER_SERVICE_LOG_GET_ALL_USERS);
+        return StreamSupport.stream(userRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
+    @Cacheable(value = USER_CACHE_NAME, key = "#id")
+    @Override
+    public User findById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(
+                        () -> new ServiceException(String.format(USER_SERVICE_EXCEPTION_NOT_EXIST_USER, id))
+                );
+        log.info(USER_SERVICE_LOG_GET_USER, user);
+        return user;
+    }
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    @Caching(evict= {@CacheEvict(value= USER_CACHE_NAME, key= "#id")})
+    @Override
+    public void deleteById(Long id) {
+        User userForDelete = self.findById(id);
+
+        if((excursionService.userTicketsCount(id) > 0) || (userForDelete.getCoins() != 0)){
+            throw new ServiceException(String.format(USER_SERVICE_EXCEPTION_USER_HAVE_TICKETS_OR_COINS, id));
+        }
+
+        userRepository.deleteUser(id, userForDelete.getCoinsLastUpdate());
+        log.info(USER_SERVICE_LOG_DELETE_USER, userForDelete);
+    }
+
+    @Caching(put= {@CachePut(value= USER_CACHE_NAME, key= "#result.id")})
+    @Override
+    public User coinsDownByExcursion(Long id, Long coins) {
+        User updatedUser = updateCoins(id, coins, IS_NOT_UP_FLAG);
+        log.info(USER_SERVICE_LOG_DOWN_COINS_BY_EXCURSION, id, coins);
+        return updatedUser;
+    }
+
+    @Caching(put= {@CachePut(value= USER_CACHE_NAME, key= "#result.id")})
+    @Override
+    public User coinsDownByUser(Long id, Long coins) {
+        User updatedUser = updateCoins(id, coins, IS_NOT_UP_FLAG);
+        log.info(USER_SERVICE_LOG_DOWN_COINS_BY_USER, id, coins);
+        return updatedUser;
+    }
+
+    @Caching(put= {@CachePut(value= USER_CACHE_NAME, key= "#result.id")})
+    @Override
+    public User coinsUpByExcursion(Long id, Long coins) {
+        User updatedUser = updateCoins(id, coins, IS_UP_FLAG);
+        log.info(USER_SERVICE_LOG_UP_COINS_BY_EXCURSION, id, coins);
+        return updatedUser;
+    }
+
+    @Caching(put= {@CachePut(value= USER_CACHE_NAME, key= "#result.id")})
+    @Override
+    public User coinsUpByUser(Long id, Long coins) {
+        User updatedUser = updateCoins(id, coins, IS_UP_FLAG);
+        log.info(USER_SERVICE_LOG_UP_COINS_BY_USER, id, coins);
+        return updatedUser;
+    }
+
+    private User updateCoins(Long id, Long coins, boolean isUp) {
+        User user = self.findById(id);
+
+        Long oldCoins = user.getCoins();
+        Long newCoins = getNewCoins(coins, isUp, oldCoins);
+
+        LocalDateTime oldCoinsLastUpdate = user.getCoinsLastUpdate();
+        LocalDateTime newCoinsLastUpdate = LocalDateTime.now();
+
+        if(userRepository.updateCoins(id, oldCoins, oldCoinsLastUpdate, newCoins, newCoinsLastUpdate) == 0) {
+            throw new ServiceException(String.format(USER_SERVICE_EXCEPTION_WRONG_COINS_UPDATE, user));
+        }
+
+        user.setCoins(newCoins);
+        user.setCoinsLastUpdate(newCoinsLastUpdate);
+        return user;
+    }
+
+    private Long getNewCoins(Long coins, boolean isUp, Long currentCoins) {
+        Long newCoins = currentCoins;
+        boolean needException = false;
+        if(coins == null) {
+            needException = true;
+        } else if(coins <= 0) {
+            needException = true;
+        } else {
+            if (isUp) {
+                if ((Long.MAX_VALUE - currentCoins) < coins) {
+                    needException = true;
+                }
+                newCoins += coins;
+            } else {
+                if ((currentCoins - coins) < 0l) {
+                    needException = true;
+                }
+                newCoins -= coins;
+            }
+        }
+
+        if(needException) {
+            throw new ServiceException(USER_SERVICE_EXCEPTION_WRONG_COINS_ARGS);
+        }
+        return newCoins;
+    }
+
+    private User saveOrUpdateUtil(User userForSave) {
+        User savedUser;
+        try {
+            savedUser = userRepository.save(userForSave);
+        } catch (ConstraintViolationException e) {
+            throw new ServiceException(e.getConstraintViolations().iterator().next().getMessage());
+        } catch (Exception e) {
+            throw new ServiceException(e.getMessage());
+        }
+        return savedUser;
+    }
+}
